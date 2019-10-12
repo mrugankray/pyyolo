7fcdab3e37e37df5149076548367915a39a31767
mrugankray <mrugankray2@gmail.com>
2019-10-12 22:05:29 +0530

    added img_class in sample

--- library/utils.py
+++ library/utils.py
@@ -454,7 +454,8 @@ rootDirCoord = '/media/mrugank/626CB0316CB00239/for development purpose only/pyt
 
 num_of_pics = 1
 
-'''for i in range(0, num_of_pics):
+'''
+for i in range(0, num_of_pics):
     idx = np.random.randint(0, len(dataset))
     #idx = 0
     sample = dataset[idx]
@@ -462,14 +463,14 @@ num_of_pics = 1
     #print('img name:',sample['img_name'])
     print("sample['grid_locate_x']", sample['grid_locate_x'])
     print('tensor:', sample['coord'][sample['grid_locate_y']][sample['grid_locate_x']])
-    print('img_class',sample['img_class'])'''
-    '''
+    print('img_class',sample['img_class'])
+    
     sample['grid_locate_x'] 4
     tensor: [-2.     -2.     -2.     -2.     -2.     -2.     -2.     -1.98   -1.98
     -2.     -1.69   -1.97   -1.7765 -1.855 ]
     img_class 1
 
-    '''
+'''
     #showCenter(img = sample['image'], xmin = sample['coord'][0],xmax = sample['coord'][1],ymin = sample['coord'][2],ymax = sample['coord'][3])
 
 '''showCenter(Imgpath = '/media/mrugank/626CB0316CB00239/for development purpose only/python/computer_vision/part_1_mod_1_lsn_2/yolo/dataset/images/dogs_imgs/dog-27.jpg',TxtrootDirCoordPath = '/media/mrugank/626CB0316CB00239/for development purpose only/python/computer_vision/part_1_mod_1_lsn_2/yolo/dataset/texts/dogs_txts/dog-27.txt')'''
